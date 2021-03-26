31317dae9cc180b63cd9790be87edf1020c0fde6
sandeep kesarwani <sandeep.kesarwani@mechademy.com>
2021-03-26 17:35:04 +0530

    UPDATE: endpoints added

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+const express = require("express");
+const app = express();
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-  async function main() {
-    while(true) {
-      console.log("Microservices rock!");
-      await sleep(5000);
-    }
-  }
-  
-  main();  
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello!");
+});
